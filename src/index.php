9969b482065ab3a04aa9f59b522ce6b523fc7e32
joeldesaulniers <60636149+joeldesaulniers@users.noreply.github.com>
2025-01-23 17:01:25 -0500

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,44 @@
+
+<html>
+<head>
+    <title>Welcome to my website!</title>
+</head>
+<body>
+    <h1>Hello World from Docker!</h1>
+
+    <a href="exercise.txt">
+        <img src="logo.png" alt="Click me!" width="125" height="125">
+    </a>
+
+    <br><br>
+
+    <form method="post">
+        <input type="submit" name="query" value="Query MongoDB"/>
+    </form>
+
+    <?php
+    use MongoDB\Driver\Manager;
+    use MongoDB\Driver\Command;
+    use MongoDB\Driver\Query;
+    use MongoDB\Driver\Cursor;
+
+    // MongoDB connection
+    $client = new MongoDB\Driver\Manager("mongodb://[username]:[password]@[hostname]:27017?authSource=[database_name]");
+
+    // Query setup
+    $filter = [];
+    $options = [];
+    $query = new MongoDB\Driver\Query($filter, $options);
+    
+    // Execute query and display results
+    $cursor = $client->executeQuery('[database_name].[collection]', $query);
+    if(isset($_POST['query'])) {
+        foreach ($cursor as $document) {
+            echo "========<br />".PHP_EOL;
+            var_dump($document);
+            echo "<br />".PHP_EOL;
+        }
+    }
+    ?>
+</body>
+</html>
