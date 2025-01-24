0aa449007cc2bf6d3e496907d8431a33da59c9d1
Joel Desaulniers <60636149+joeldesaulniers@users.noreply.github.com>
2025-01-24 15:08:44 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+# Terraform Configuration Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.4.2"
+}
+
+# AWS Provider Configuration
+provider "aws" {
+  region = "ca-central-1"
+
+  default_tags {
+    tags = {
+      Environment = "sandbox"
+      Terraform   = "true"
+      Project     = "infrastructure"
+    }
+  }
+}
